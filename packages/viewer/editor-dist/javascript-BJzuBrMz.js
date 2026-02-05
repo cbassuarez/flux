70788405eb9011a2c83ec786359d4426164b633b
Seb Suarez <contact@cbassuarez.com>
2026-02-05 14:01:44 -0800

    Implement phase 2 Flux editor

--- packages/viewer/editor-dist/javascript-BJzuBrMz.js
+++ packages/viewer/editor-dist/javascript-BJzuBrMz.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-B-Vha2El.js";import"./index-B_DsvRQm.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-DSnKvFmJ.js";import"./index-Dfzrmyw0.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
